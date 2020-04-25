2a54be17d41e5dc3431de9e4ab5ce16d3e44bdf9
toasiva <siva.arumugham@cognizant.com>
2020-04-26 00:28:07 +0530

    CloudFoundry

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private String strMessage = null;
 
+
+    public WelcomeController(@Value("${welcome.message}") String arg1){
+        this.strMessage = arg1;
+    }
+    
     @GetMapping("/")
     public String sayHello(){
-        return "Say Hello";
+        return strMessage;
     }
 
 }
+
